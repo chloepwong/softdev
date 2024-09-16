939f3302a620a1831af09c908863357a72a29125
Chloe Wong <144498614+chloepwong@users.noreply.github.com>
2024-09-16 11:20:13 -0400

    added 04_rando_dict

---
+++ 04_rando_dict/krewes.py
@@ -0,0 +1,28 @@
+# Chloe Wong
+# Team X
+# SoftDev
+# K04 -- Random Devo/Python Dictionaries & Random Selection/Select random devo from dictionary
+# 2024-09-13
+# time spent: 0.15
+
+import random
+
+krewes = {
+           4: [ 
+		'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+		'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+		'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+		'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+		],
+           5: [ 
+                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
+              ]
+         }
+
+def pickDevo(devos):
+    print(random.choice(random.choice(list(devos.values()))))
+          
+pickDevo(krewes)
