79e636bb8ea8e3da79e90a0ce547cef1a1c9089a
Chloe Wong <144498614+chloepwong@users.noreply.github.com>
2024-09-20 23:09:28 -0400

    updated numbercruncher.py

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,1 +1,29 @@
+# Tiffany Yang, Chloe Wong
+# Team X
+# SoftDev
+# K05 -- Adding more info to lists/dicts
+# 2024-09-19
+# time spent: 1 hour
 
+import random
+
+occupations = []  
+
+def createList():
+    with open("06_py-csv/occupations.csv", "r") as file:
+        data = file.read().split("\n")
+        for job in data:
+            percent= job.split(",")
+            occupations.append({"job": percent[0]})
+
+def pickJob():
+    createList()  
+    if len(occupations) == 0:
+        print("The list is empty!")
+        return
+    l = int(len(occupations)//2 - 1)
+    choice = random.randint(0, l)
+    job= occupations[choice]
+    print(job)
+
+pickJob()
