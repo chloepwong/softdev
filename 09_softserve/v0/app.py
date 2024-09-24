e723ebe0ff9b94735ee3cd3ae71983b91c2951ba
Chloe Wong <144498614+chloepwong@users.noreply.github.com>
2024-09-24 11:24:26 -0400

    updated v0

--- 09_softserve/v0/app.py
+++ 09_softserve/v0/app.py
@@ -3,15 +3,15 @@
 # September 2024
 
 # This is the same code from K08.
-# It leads to a site that states "No hablo queso!" and prints __main__ in the terminal.
+# It leads to a site that states "No hablo queso!" and prints __main__ in the console.
 
 from flask import Flask
-app = Flask(__name__)          # ...
+app = Flask(__name__)          # Create instance of class Flask
 
-@app.route("/")                # ...
+@app.route("/")                # Assign function to route
 def hello_world():
-    print(__name__)            # ...
-    return "No hablo queso!"   # ...
+    print(__name__)            # Print __main__ in console
+    return "No hablo queso!"   # Write "No hablo queso!" in site
 
-app.run()                      # ...
+app.run()                      # Run application
                 
