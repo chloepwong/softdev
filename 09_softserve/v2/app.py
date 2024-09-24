38d0b29a912717ee467096a6b7633ab603f56567
Chloe Wong <144498614+chloepwong@users.noreply.github.com>
2024-09-24 11:29:52 -0400

    updated v2

--- 09_softserve/v2/app.py
+++ 09_softserve/v2/app.py
@@ -2,13 +2,17 @@
 # SoftDev
 # September 2024
 
+# This is the same code as that of v0, except it also prints "about to print __name__..." in the console
+# It will show "No hablo queso!" in the site
+# It will print "about to print __name__..." then __main__ in the console
+
 from flask import Flask
 app = Flask(__name__)             #create instance of class Flask
 
 @app.route("/")                   #assign fxn to route
 def hello_world():
     print("about to print __name__...")
-    print(__name__)               #where will this go?
+    print(__name__)               #this goes to the console
     return "No hablo queso!"
 
 app.run()
