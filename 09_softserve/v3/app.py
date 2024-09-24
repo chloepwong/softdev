83b561ed386e2d2bcfa2c41b239d091f2b5d5c01
Chloe Wong <144498614+chloepwong@users.noreply.github.com>
2024-09-24 11:35:48 -0400

    updated v3

--- 09_softserve/v3/app.py
+++ 09_softserve/v3/app.py
@@ -2,13 +2,17 @@
 # SoftDev
 # September 2024
 
+# This code adds app.debug = True
+# I expect it to have similar behavior to previous code but with some debugging mechanism
+# The page automatically reloads with code updates and gives errors when applicable
+
 from flask import Flask
 app = Flask(__name__)                 #create instance of class Flask
 
 @app.route("/")                       #assign fxn to route
 def hello_world():
     print("about to print __name__...")
-    print(__name__)                   #where will this go?
+    print(__name__)                   #this goes to the console
     return "No hablo queso!"
 
 app.debug = True
