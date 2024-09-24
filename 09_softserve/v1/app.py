b08b45307ce0794f39287424cbe2dd42c91a1c70
Chloe Wong <144498614+chloepwong@users.noreply.github.com>
2024-09-24 11:26:30 -0400

    updated v1

--- 09_softserve/v1/app.py
+++ 09_softserve/v1/app.py
@@ -2,6 +2,9 @@
 # SoftDev
 # September 2024
 
+# This code is the same as that of v0, except it excludes print(__name__)
+# It will write "No hablo queso!" in the site but not print __main__ in the console
+
 from flask import Flask
 app = Flask(__name__)            #create instance of class Flask
 
