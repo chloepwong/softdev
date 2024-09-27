9e838fd43d76ed4ae5e104632cfbf3f8d78e83b4
Chloe Wong <144498614+chloepwong@users.noreply.github.com>
2024-09-27 11:27:56 -0400

    updated app.py

--- 12_flask-jinja/app.py
+++ 12_flask-jinja/app.py
@@ -8,11 +8,13 @@ Q0:
 render_template is used in test_templt. 
 I expect there to be an error message when accessing 
 http://127.0.0.1:5000/my_foist_template 
-but not http://127.0.0.1:5000/my_foist_template.
+but not http://127.0.0.1:5000.
+These predictions were correct.
 
 Q1:
 "/my_foist_template" is in @app.route().
 We predict the page can be loaded on http://127.0.0.1:5000/my_foist_template,
+This was correct.
 
 Q2:
 The first argument gives 'model_tmplt.html' as the template file.
@@ -27,7 +29,7 @@ The third arguments sets coll as the variable collection.
 # Q0: What will happen if you remove render_template from the following statement?
 # (log prediction before executing...)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
