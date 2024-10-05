85c2dae439af0d5db62f51dddefe5985954e9e67
chloepwong <cwong50@stuy.edu>
2024-10-05 16:07:20 -0400

    updated app.py

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -22,7 +22,7 @@ def randomjob():
         jobs[columns[0]].append(row[columns[0]])
         jobs[columns[1]].append(float(row[columns[1]]))
         jobs[columns[2]].append(row[columns[2]])
-    rand_job = random.choices(jobs[columns[0]], weights=jobs[columns[1]], k=1)[0]
+    rand_job = random.choices(jobs[columns[0]], weights=jobs[columns[1]])[0]
     return render_template('tablified.html', job_info = jobs, categories = columns, job = rand_job)
 
 
