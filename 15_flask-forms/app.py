1a2f0a0aa3405c35ef688f6b0f381839b73f00f9
chloepwong <cwong50@stuy.edu>
2024-10-08 20:57:25 -0400

    updated 15_flask-forms

--- 15_flask-forms/app.py
+++ 15_flask-forms/app.py
@@ -1,23 +1,16 @@
-# Chloe Wong
+# Chloe Wong, Stella Yampolsky, Jessica Yu
 # :(
 # SoftDev
 # K15 - More flask forms!
 # 2024-10-8
-# Time Spent: 
+# Time Spent: 0.5
 
 # import conventions:
 # list most general first (standard python library)
 # ...then pip installs (eg Flask)
 # ...then your own home-rolled modules/packages (today's test module)
 
-from flask import Flask             #facilitate flask webserving
-from flask import render_template   #facilitate jinja templating
-from flask import request           #facilitate form submission
-
-import testmod0
-
-#the conventional way:
-#from flask import Flask, render_template, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)    #create Flask object
 
@@ -38,7 +31,7 @@ def disp_loginpage():
 
 
 @app.route("/auth") # , methods=['GET', 'POST'])
-def authenticate():
+def response():
     #print("\n\n\n")
     #print("***DIAG: this Flask obj ***")
     #print(app)
@@ -50,8 +43,7 @@ def authenticate():
     #print(request.args['username'])
     #print("***DIAG: request.headers ***")
     #print(request.headers)
-    return "Waaaa hooo HAAAH"  #response to a form submission
-
+    return render_template( 'response.html', response=request.args['username'], method=request.method )
 
     
 if __name__ == "__main__": #false if this file imported as module
