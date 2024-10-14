fe79491853acfeebbced26480784beec51f095a0
chloepwong <cwong50@stuy.edu>
2024-10-13 20:57:49 -0400

    updated 16_flask-sessions

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -3,31 +3,31 @@
 # SoftDev
 # K15 -- user sessions
 # 2024-10-9
-# Time Spent:
+# Time Spent: 1 hr
 
 import os
 
-from flask import Flask, render_template, request, session, url_for
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)    #create Flask object
 
 app.secret_key = os.urandom(32)
 
-@app.route("/") #, methods=['GET', 'POST'])
-def disp_loginpage():
+@app.route("/", methods=['GET', 'POST'])
+def home():
     if 'username' in session:
         return render_template( 'response.html', response=session['username'], method=request.method)
     return render_template( 'login.html' )
 
 
-@app.route("/auth") # , methods=['GET', 'POST'])
-def authenticate():
-    session['username'] = request.args['username']
-    return redirect(url_for('disp_loginpage'))
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    session['username'] = request.form['username']
+    return redirect(url_for('home'))
 
-@app.route("/logout") # , methods=['GET', 'POST'])
+@app.route("/logout", methods=['GET', 'POST'])
 def logout():
-    session.pop('username')
+    session.pop('username', None)
     return render_template( 'logout.html' )
 
     
