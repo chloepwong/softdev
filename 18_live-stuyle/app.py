6935ddf2b85e565e9d6645747e82c8a9aa9429ee
chloepwong <cwong50@stuy.edu>
2024-10-17 05:48:01 -0400

    updated 18_live-stuyle

--- 18_live-stuyle/app.py
+++ 18_live-stuyle/app.py
@@ -1,1 +1,17 @@
+# Chloe Wong, Tiffany Yang, Claire Song
+# Team X
+# SoftDev
+# K17 - opening HTML/CSS in flask app
+# 2024-10-16
+# Time Spent: 0.1 hr
 
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html', style='static/style.css')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
