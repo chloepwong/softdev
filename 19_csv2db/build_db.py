58dc941a1bc311656e8a2ffd1ccad40188c5740c
chloepwong <cwong50@stuy.edu>
2024-10-21 03:28:28 -0400

    updated build_db.py

--- 19_csv2db/build_db.py
+++ 19_csv2db/build_db.py
@@ -3,7 +3,7 @@
 # SoftDev
 # K19 - databases
 # 2024-10-18
-# Time Spent:
+# Time Spent: 1 hr
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -16,14 +16,43 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 #==========================================================
 
+studentcsv = csv.DictReader(open('students.csv', newline=''))
+coursecsv = csv.DictReader(open('courses.csv', newline=''))
 
-"""
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-< < < INSERT YOUR TEAM'S DB-POPULATING CODE HERE > > >
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-"""
+allcourses = []
+uniquecourses = set()
 
-command = ""          # test SQL stmt in sqlite3 shell, save as string
+for course in coursecsv:
+    allcourses.append(course)
+    uniquecourses.add(course['code'])
+    
+uniquecourses = list(uniquecourses)
+
+allstudents = []
+
+for student in studentcsv:
+    studentinfo = {
+        'name': student['name'],
+        'id': student['id'],
+        'age': student['age']
+    }
+    studentscourses = []
+    for course in allcourses:
+        if course['id'] == student['id']:
+            studentinfo[course['code']] = course['mark']
+            studentscourses.append(course['code'])
+    for course in uniquecourses:
+        if course not in studentscourses:
+            studentinfo[course] = 'NULL'
+    allstudents.append(studentinfo)
+
+c.execute("DROP TABLE IF EXISTS school")
+c.execute(f"CREATE TABLE school(name, id, age, {uniquecourses[0]}, {uniquecourses[1]}, {uniquecourses[2]}, {uniquecourses[3]}, {uniquecourses[4]})")
+
+for student in allstudents:
+    c.execute(f"INSERT INTO school VALUES ('{student['name']}', {student['id']}, {student['age']}, {student[uniquecourses[0]]}, {student[uniquecourses[1]]}, {student[uniquecourses[2]]}, {student[uniquecourses[3]]}, {student[uniquecourses[4]]})")
+    
+command = "SELECT * FROM school"          # test SQL stmt in sqlite3 shell, save as string
 c.execute(command)    # run SQL statement
 
 #==========================================================
